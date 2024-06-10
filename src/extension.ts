8e76f5b6dbe8d0ffdd3a33c5a4ab07974db622a0
Aryan Pingle <realaryanpingle@gmail.com>
2024-06-10 16:57:01 +0530

    Add basic webview editor

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,19 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	// Custom editor window that opens a path visualization
+	context.subscriptions.push(
+		vscode.commands.registerCommand('import-graph.pathviz', () => {
+			const panel = vscode.window.createWebviewPanel(
+				'customType', // Identifies the type of the webview. Used internally
+				'Import Graph', // Title of the panel displayed to the user
+				vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+				{} // Webview options. More on these later.
+			);
+		})
+	);
+
 	// Simple notification command for debugging
 	context.subscriptions.push(
 		vscode.commands.registerCommand('import-graph.helloWorld', () => {
