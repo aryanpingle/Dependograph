51810d4a41429fcfeb72b1b9388a59fdd7bd9d4f
Aryan Pingle <realaryanpingle@gmail.com>
2024-06-10 15:25:23 +0530

    Add activity bar icon

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,12 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "import-graph" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('import-graph.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from import-graph!');
-	});
-
-	context.subscriptions.push(disposable);
+	// Simple notification command for debugging
+	context.subscriptions.push(
+		vscode.commands.registerCommand('import-graph.helloWorld', () => {
+			vscode.window.showInformationMessage('Hello, world!');
+		})
+	);
 }
 
 // This method is called when your extension is deactivated
