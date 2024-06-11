a1be8147ce32b223e57357d75e250ae9eb81149d
Aryan Pingle <realaryanpingle@gmail.com>
2024-06-11 20:46:57 +0530

    Prettify files

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,22 @@
 // TODO: Analyse dependencies in a child process
 
-import * as vscode from 'vscode';
-import { onPathVisualization } from './path-visualizer';
+import * as vscode from "vscode";
+import { onPathVisualization } from "./path-visualizer";
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(
-		vscode.commands.registerCommand('import-graph.pathviz', onPathVisualization)
-	);
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            "import-graph.pathviz",
+            onPathVisualization,
+        ),
+    );
 
-	// Simple notification command for debugging
-	context.subscriptions.push(
-		vscode.commands.registerCommand('import-graph.helloWorld', () => {
-			vscode.window.showInformationMessage('Hello, world!');
-		})
-	);
+    // Simple notification command for debugging
+    context.subscriptions.push(
+        vscode.commands.registerCommand("import-graph.helloWorld", () => {
+            vscode.window.showInformationMessage("Hello, world!");
+        }),
+    );
 }
 
 export function deactivate() {}
--- src/path-visualizer.ts
+++ src/path-visualizer.ts
@@ -1,10 +1,10 @@
-import * as vscode from 'vscode';
-import dependencyTree from 'dependency-tree';
-import path from 'path';
+import * as vscode from "vscode";
+import dependencyTree from "dependency-tree";
+import path from "path";
 
 export async function onPathVisualization() {
     const currentWorkspace = vscode.workspace?.workspaceFolders?.[0].uri.fsPath;
-    if(!currentWorkspace) {
+    if (!currentWorkspace) {
         vscode.window.showErrorMessage(`No workspace found.`);
         return;
     }
@@ -12,17 +12,19 @@ export async function onPathVisualization() {
     const selectedFiles = await vscode.window.showOpenDialog({
         canSelectMany: false,
         title: "Select an entry file",
-        openLabel: "Use as entry file"
-    })
-    if(selectedFiles === undefined) {
+        openLabel: "Use as entry file",
+    });
+    if (selectedFiles === undefined) {
         // Cancelled
         return;
     }
 
     const entryFileURI = selectedFiles[0];
 
-    if(path.dirname(entryFileURI.fsPath) !== currentWorkspace) {
-        vscode.window.showErrorMessage("Entry file must be contained within the current workspace.");
+    if (path.dirname(entryFileURI.fsPath) !== currentWorkspace) {
+        vscode.window.showErrorMessage(
+            "Entry file must be contained within the current workspace.",
+        );
         return;
     }
 
@@ -34,9 +36,9 @@ export async function onPathVisualization() {
     console.log(dependencyGraph);
 
     const panel = vscode.window.createWebviewPanel(
-        'customType',
-        'Import Graph',
+        "customType",
+        "Import Graph",
         vscode.ViewColumn.One,
-        {} 
+        {},
     );
 }
