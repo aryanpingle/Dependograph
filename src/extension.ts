1604959e32c9c98223f29b01a542be25c1c33bba
Aryan Pingle <realaryanpingle@gmail.com>
2024-06-11 20:17:34 +0530

    Refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -1,49 +1,11 @@
 // TODO: Analyse dependencies in a child process
 
 import * as vscode from 'vscode';
-import dependencyTree from 'dependency-tree';
-import path from 'path';
+import { onPathVisualization } from './path-visualizer';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
-		vscode.commands.registerCommand('import-graph.pathviz', async () => {
-			const currentWorkspace = vscode.workspace?.workspaceFolders?.[0].uri.fsPath;
-			if(!currentWorkspace) {
-				vscode.window.showErrorMessage(`No workspace found.`);
-				return;
-			}
-
-			const selectedFiles = await vscode.window.showOpenDialog({
-				canSelectMany: false,
-				title: "Select an entry file",
-				openLabel: "Use as entry file"
-			})
-			if(selectedFiles === undefined) {
-				// Cancelled
-				return;
-			}
-
-			const entryFileURI = selectedFiles[0];
-
-			if(path.dirname(entryFileURI.fsPath) !== currentWorkspace) {
-				vscode.window.showErrorMessage("Entry file must be contained within the current workspace.");
-				return;
-			}
-
-			const dependencyGraph = dependencyTree({
-				directory: currentWorkspace,
-				filename: entryFileURI.fsPath,
-			});
-			// Log the dependency graph for now
-			console.log(dependencyGraph);
-
-			const panel = vscode.window.createWebviewPanel(
-				'customType',
-				'Import Graph',
-				vscode.ViewColumn.One,
-				{} 
-			);
-		})
+		vscode.commands.registerCommand('import-graph.pathviz', onPathVisualization)
 	);
 
 	// Simple notification command for debugging
---
+++ src/path-visualizer.ts
@@ -0,0 +1,42 @@
+import * as vscode from 'vscode';
+import dependencyTree from 'dependency-tree';
+import path from 'path';
+
+export async function onPathVisualization() {
+    const currentWorkspace = vscode.workspace?.workspaceFolders?.[0].uri.fsPath;
+    if(!currentWorkspace) {
+        vscode.window.showErrorMessage(`No workspace found.`);
+        return;
+    }
+
+    const selectedFiles = await vscode.window.showOpenDialog({
+        canSelectMany: false,
+        title: "Select an entry file",
+        openLabel: "Use as entry file"
+    })
+    if(selectedFiles === undefined) {
+        // Cancelled
+        return;
+    }
+
+    const entryFileURI = selectedFiles[0];
+
+    if(path.dirname(entryFileURI.fsPath) !== currentWorkspace) {
+        vscode.window.showErrorMessage("Entry file must be contained within the current workspace.");
+        return;
+    }
+
+    const dependencyGraph = dependencyTree({
+        directory: currentWorkspace,
+        filename: entryFileURI.fsPath,
+    });
+    // Log the dependency graph for now
+    console.log(dependencyGraph);
+
+    const panel = vscode.window.createWebviewPanel(
+        'customType',
+        'Import Graph',
+        vscode.ViewColumn.One,
+        {} 
+    );
+}
