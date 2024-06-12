7a8fe1d8dd324d35e124bca62587bb63d749d1f2
Aryan Pingle <realaryanpingle@gmail.com>
2024-06-12 11:48:27 +0530

    Add basic webview

--- src/path-visualizer.ts
+++ src/path-visualizer.ts
@@ -3,6 +3,8 @@ import dependencyTree from "dependency-tree";
 import path from "path";
 
 export async function onPathVisualization() {
+    const isDevelopment = process.env.VSCODE_DEBUG_MODE === "true";
+
     const currentWorkspace = vscode.workspace?.workspaceFolders?.[0].uri.fsPath;
     if (!currentWorkspace) {
         vscode.window.showErrorMessage(`No workspace found.`);
@@ -31,9 +33,14 @@ export async function onPathVisualization() {
     const dependencyGraph = dependencyTree({
         directory: currentWorkspace,
         filename: entryFileURI.fsPath,
+        // TODO: Inefficient. Custom parser should ignore by default.
+        filter: (path: string): boolean => {
+            if(path.includes("node_modules")) return false;
+            return true;
+        }
     });
     // Log the dependency graph for now
-    console.log(dependencyGraph);
+    // console.log(dependencyGraph);
 
     const panel = vscode.window.createWebviewPanel(
         "customType",
@@ -41,4 +48,31 @@ export async function onPathVisualization() {
         vscode.ViewColumn.One,
         {},
     );
+    const params: WebviewParams = {
+        dependencyGraph: dependencyGraph,
+    };
+    panel.webview.html = getWebviewContent(params);
+}
+
+interface WebviewParams {
+    dependencyGraph: Object;
+}
+
+function getWebviewContent(params: WebviewParams): string {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <input type="text">
+    <p>
+        ${JSON.stringify(params.dependencyGraph, undefined, 4).replaceAll("\n", "<br>").replaceAll(" ", "&nbsp;")}
+    </p>
+</body>
+</html>
+    `;
 }
