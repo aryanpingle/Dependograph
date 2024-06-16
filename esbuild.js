438067564563447e3e59ab17cc067ceb35dc4fe2
Aryan Pingle <realaryanpingle@gmail.com>
2024-06-16 20:13:13 +0530

    Improve esbuild

--- esbuild.js
+++ esbuild.js
@@ -1,55 +1,79 @@
-const esbuild = require('esbuild');
+const esbuild = require("esbuild");
 
-const production = process.argv.includes('--production');
-const watch = process.argv.includes('--watch');
+const production = process.argv.includes("--production");
+const watch = process.argv.includes("--watch");
 
 async function main() {
-  const ctx = await esbuild.context({
-    entryPoints: ['src/extension.ts', 'src/webviews/visualization.ts', 'src/webviews/sidebar.ts'],
-    bundle: true,
-    format: 'cjs',
-    minify: production,
-    sourcemap: !production,
-    sourcesContent: false,
-    platform: 'node',
-    outdir: 'out',
-    external: ['vscode', 'dependency-tree'],
-    logLevel: 'silent',
-    plugins: [
-      /* add to the end of plugins array */
-      esbuildProblemMatcherPlugin
-    ],
-    allowOverwrite: true,
-  });
-  if (watch) {
-    await ctx.watch();
-  } else {
-    await ctx.rebuild();
-    await ctx.dispose();
-  }
+    const ctxExtension = await esbuild.context({
+        entryPoints: ["src/extension.ts"],
+        bundle: true,
+        format: "cjs",
+        minify: production,
+        sourcemap: !production,
+        sourcesContent: false,
+        platform: "node",
+        outdir: "out",
+        external: ["vscode", "dependency-tree"],
+        logLevel: "silent",
+        plugins: [
+            /* add to the end of plugins array */
+            esbuildProblemMatcherPlugin,
+        ],
+        allowOverwrite: true,
+    });
+    const ctxWebviews = await esbuild.context({
+        entryPoints: [
+            "src/webviews/visualization.ts",
+            "src/webviews/sidebar.ts",
+        ],
+        bundle: true,
+        format: "esm",
+        target: "es2020",
+        minify: production,
+        sourcemap: !production,
+        sourcesContent: false,
+        outdir: "out/webviews",
+        logLevel: "silent",
+        plugins: [
+            /* add to the end of plugins array */
+            esbuildProblemMatcherPlugin,
+        ],
+        allowOverwrite: true,
+    });
+
+    if (watch) {
+        await Promise.allSettled([ctxExtension.watch(), ctxWebviews.watch()]);
+    } else {
+        await Promise.allSettled([
+            ctxExtension.rebuild().then(ctxExtension.dispose),
+            ctxWebviews.rebuild().then(ctxWebviews.dispose),
+        ]);
+    }
 }
 
 /**
  * @type {import('esbuild').Plugin}
  */
 const esbuildProblemMatcherPlugin = {
-  name: 'esbuild-problem-matcher',
+    name: "esbuild-problem-matcher",
 
-  setup(build) {
-    build.onStart(() => {
-      console.log('[watch] build started');
-    });
-    build.onEnd(result => {
-      result.errors.forEach(({ text, location }) => {
-        console.error(`✘ [ERROR] ${text}`);
-        console.error(`    ${location.file}:${location.line}:${location.column}:`);
-      });
-      console.log('[watch] build finished');
-    });
-  }
+    setup(build) {
+        build.onStart(() => {
+            console.log("[watch] build started");
+        });
+        build.onEnd((result) => {
+            result.errors.forEach(({ text, location }) => {
+                console.error(`✘ [ERROR] ${text}`);
+                console.error(
+                    `    ${location.file}:${location.line}:${location.column}:`,
+                );
+            });
+            console.log("[watch] build finished");
+        });
+    },
 };
 
-main().catch(e => {
-  console.error(e);
-  process.exit(1);
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
 });
