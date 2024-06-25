0f572974b0b434af0957decd6e6840eeee13ae00
aryanpingle <realaryanpingle@gmail.com>
2024-06-25 13:46:38 +0530

    Use the vscode webview ui toolkit

--- src/webviews/settings-panel.ts
+++ src/webviews/settings-panel.ts
@@ -19,6 +19,11 @@ export function setupSettingsPanel() {
     addSettingsListeners();
 }
 
+let config = {
+    hideModules: false,
+    hideFilenames: false,
+};
+
 /**
  * Toggle the settings panel to be shown or not.
  */
@@ -33,25 +38,44 @@ function addSettingsListeners() {
         "#checkbox-hide_modules",
     ) as HTMLInputElement;
     checkboxHideModules.addEventListener("change", function () {
-        let { nodes, links } = new Graph(dependencyInfo).getNodesAndLinks();
-
-        if (this.checked) {
-            // Remove node modules
-            const NodeModuleNodeIds = new Set<string>();
-            nodes.forEach((node) => {
-                if (node.fileType === "nodejs") {
-                    NodeModuleNodeIds.add(node.id);
-                }
-            });
-            nodes = nodes.filter((node) => !NodeModuleNodeIds.has(node.id));
-            links = links.filter((link) => {
-                return (
-                    !NodeModuleNodeIds.has(link.source as string) &&
-                    !NodeModuleNodeIds.has(link.target as string)
-                );
-            });
-        }
-
-        setupVisualization(nodes, links);
+        config.hideModules = this.checked;
+        setupVisualizationConfig();
     });
+
+    const checkboxHideFilenames = document.querySelector(
+        "#checkbox-hide_filenames",
+    ) as HTMLInputElement;
+    checkboxHideFilenames.addEventListener("change", function () {
+        config.hideFilenames = this.checked;
+        setupVisualizationConfig();
+    });
+}
+
+function setupVisualizationConfig() {
+    let { nodes, links } = new Graph(dependencyInfo).getNodesAndLinks();
+
+    if (config.hideModules) {
+        // Remove node modules
+        const NodeModuleNodeIds = new Set<string>();
+        nodes.forEach((node) => {
+            if (node.fileType === "nodejs") {
+                NodeModuleNodeIds.add(node.id);
+            }
+        });
+        nodes = nodes.filter((node) => !NodeModuleNodeIds.has(node.id));
+        links = links.filter((link) => {
+            return (
+                !NodeModuleNodeIds.has(link.source as string) &&
+                !NodeModuleNodeIds.has(link.target as string)
+            );
+        });
+    }
+
+    if(config.hideFilenames) {
+        nodes.forEach(node => {
+            node.name = "";
+        })
+    }
+
+    setupVisualization(nodes, links);
 }
