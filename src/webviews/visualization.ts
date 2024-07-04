0c360a5094cdb6a7d82da4d246087fb729e490c9
aryanpingle <realaryanpingle@gmail.com>
2024-07-04 18:53:23 +0530

    Debounce resize listener

--- src/webviews/visualization.ts
+++ src/webviews/visualization.ts
@@ -1,5 +1,6 @@
 // This file is run within the webview
 
+import debounce from "debounce";
 import { ForceDirectedVisualization } from "./force-directed-graph";
 import { DependencyInfo } from "../code-analyser";
 import { setupSettingsPanel } from "./settings-panel";
@@ -8,10 +9,12 @@ declare const dependencyInfo: DependencyInfo;
 
 let visualization: ForceDirectedVisualization;
 
-function onWindowResize() {
+function fitSVGToContainer() {
     const container = document.querySelector(".svg-container");
     visualization.resizeSVG(container.clientWidth, container.clientHeight);
 }
+
+const debouncedResizeSVG = debounce(fitSVGToContainer, 50);
 
 // Setup function
 (function () {
@@ -20,9 +23,9 @@ function onWindowResize() {
         ForceDirectedVisualization.createSingletonInstance(dependencyInfo);
 
     // Add resize listener
-    window.addEventListener("resize", onWindowResize);
+    window.addEventListener("resize", debouncedResizeSVG);
     // Resize
-    onWindowResize();
+    debouncedResizeSVG();
 
     setupSettingsPanel();
 })();
