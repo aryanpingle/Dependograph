b097f7117f506dfe31c42f52a70a8287ca7be51c
aryan.pingle <realaryanpingle@gmail.com>
2024-07-13 19:12:56 +0530

    Improve vscodeFS - Allow path aliasing

--- src/trade-analyser/index.ts
+++ src/trade-analyser/index.ts
@@ -8,6 +8,7 @@ import {
     StringLiteral,
     TemplateLiteral,
 } from "@babel/types";
+import json5 from "json5";
 
 const vscodeFS = vscode.workspace.fs;
 
@@ -55,74 +56,205 @@ async function doesUriDirectoryExist(uri: vscode.Uri) {
     }
 }
 
+type AliasPaths = Array<string | null> | null;
+type PathAliasConfig = Record<string, AliasPaths>;
+interface CompilerOptions {
+    baseUrl?: string;
+    paths?: PathAliasConfig;
+}
+
+function aliasMatchesPath(path: string, alias: string): boolean {
+    const splitAlias = alias.split("*");
+
+    // Invalid aliases are ignored
+    if (splitAlias.length > 2) return false;
+
+    const [aliasHalf1, aliasHalf2] = splitAlias;
+
+    if (aliasHalf1 && !path.startsWith(aliasHalf1)) return false;
+    if (aliasHalf2 && !path.endsWith(aliasHalf2)) return false;
+
+    return true;
+}
+
+function getDeAliasedPaths(
+    path: string,
+    baseUrl: string,
+    alias: string,
+    aliasPaths: AliasPaths,
+): string[] {
+    if (aliasPaths === null) return [];
+
+    const [aliasHalf1, aliasHalf2] = alias.split("*");
+    let half1Length = aliasHalf1 ? aliasHalf1.length : 0;
+    let half2Length = aliasHalf2 ? aliasHalf2.length : 0;
+
+    const starredValue = path.substring(half1Length, path.length - half2Length);
+
+    const deAliasedPaths = aliasPaths?.map((aliasPath) => {
+        return baseUrl + "/" + aliasPath.replace("*", starredValue);
+    });
+    return deAliasedPaths;
+}
+
+/**
+ * Get all modifications of the path after using the given pathAliasConfig
+ * @param path
+ * @param pathAliasConfig
+ * @returns
+ */
+function getAllDeAliasedPaths(
+    path: string,
+    compilerOptions: CompilerOptions,
+): string[] {
+    const baseUrl = compilerOptions.baseUrl?.replace(/\/+$/g, "") ?? ".";
+    const pathAliasConfig = compilerOptions.paths ?? {};
+
+    const allPossibleDeAliasedPaths = [];
+    for (const alias in pathAliasConfig) {
+        // If the path doesn't match the alias
+        if (!aliasMatchesPath(path, alias)) continue;
+        // If it's an exact alias but the path doesn't match exactly
+        if (!alias.includes("*") && path !== alias) continue;
+
+        const deAliasedPaths = getDeAliasedPaths(
+            path,
+            baseUrl,
+            alias,
+            pathAliasConfig[alias],
+        );
+        allPossibleDeAliasedPaths.push(...deAliasedPaths);
+    }
+
+    return allPossibleDeAliasedPaths;
+}
+
+async function bruteForceFile(
+    uri: vscode.Uri,
+): Promise<vscode.Uri | undefined> {
+    const attemptExtensions = [".tsx", ".ts", ".jsx", ".js", ".d.ts"];
+    for (const extension of attemptExtensions) {
+        const fileUri = vscode.Uri.parse(uri.toString() + extension);
+        if (await doesUriExist(fileUri)) {
+            return fileUri;
+        }
+    }
+    return undefined;
+}
+
+async function vscodeResolveRelativePath(
+    baseUri: vscode.Uri,
+    relativePath: string,
+): Promise<vscode.Uri | undefined> {
+    const simpleJoinedUri = vscode.Uri.joinPath(baseUri, relativePath);
+
+    if (await doesUriExist(simpleJoinedUri)) {
+        const status = await vscodeFS.stat(simpleJoinedUri);
+        if (status.type === vscode.FileType.File) {
+            // Direct file
+            return simpleJoinedUri;
+        } else if (status.type === vscode.FileType.Directory) {
+            // Must be some index.xyz file
+            // TODO: Could be a package.json either
+
+            return bruteForceFile(
+                vscode.Uri.joinPath(simpleJoinedUri, "index"),
+            );
+        }
+        // TODO: Handle symbolic links
+
+        return undefined;
+    }
+
+    // URI does not exist, could be the uri + some extension
+    const bruted = bruteForceFile(simpleJoinedUri);
+    return bruted;
+}
+
 // TODO: Handle absolute paths (and relative paths better)
 async function vscodeResolve(
     baseUri: vscode.Uri,
     path: string,
+    compilerOptions: CompilerOptions,
 ): Promise<string> {
-    if (!path.startsWith("."))
-        return vscode.Uri.from({
+    if (!path.startsWith(".")) {
+        // Not a relative path - it's either a node module or an aliased path
+
+        // TODO: Absolute paths are rarely used, but should still be supported.
+        // Maybe check if it's a local fs, and then allow vscodeFS to access it?
+
+        const deAliasedPaths = getAllDeAliasedPaths(path, compilerOptions);
+        for (const deAliasedPath of deAliasedPaths) {
+            const deAliasedResolved = await vscodeResolveRelativePath(
+                vscode.workspace.workspaceFolders[0].uri,
+                deAliasedPath,
+            );
+            if (deAliasedResolved !== undefined) {
+                return deAliasedResolved.toString();
+            }
+        }
+
+        // Indicate that it must be a node module
+        const nodeModuleUri = vscode.Uri.from({
             scheme: "node_modules",
             path: path,
-        }).toString();
+        });
+        return nodeModuleUri.toString();
+    }
 
+    // It's a relative import, relative to the file
+    const relativeResolved = await vscodeResolveRelativePath(
+        baseUri,
+        "../" + path,
+    );
+    if (relativeResolved !== undefined) {
+        return relativeResolved.toString();
+    }
+
+    // Nothing works, so just pass it in as a simple join
     const simpleJoinedUri = vscode.Uri.joinPath(baseUri, "..", path);
-    if (await doesUriExist(simpleJoinedUri)) {
-        if (await doesUriDirectoryExist(simpleJoinedUri)) {
-            // It may be directory import (implicit /index)
-            const availableFiles =
-                await vscodeFS.readDirectory(simpleJoinedUri);
-            // TODO: Make more in line with node resolution algorithm
-            // 1. Find .ts or .tsx files
-            for (const [filename, filetype] of availableFiles) {
-                if (filetype !== vscode.FileType.File) continue;
-                if (/index\.tsx?/.test(filename)) {
-                    // Success!
-                    return vscode.Uri.joinPath(
-                        simpleJoinedUri,
-                        filename,
-                    ).toString();
-                }
-            }
-            // 2. Find .js or .jsx files
-            for (const [filename, filetype] of availableFiles) {
-                if (filetype !== vscode.FileType.File) continue;
-                if (/index\.jsx?/.test(filename)) {
-                    // Success!
-                    return vscode.Uri.joinPath(
-                        simpleJoinedUri,
-                        filename,
-                    ).toString();
-                }
-            }
-        } else {
-            // No directory = there exists a file
-            return simpleJoinedUri.toString();
-        }
-    }
-
-    // path must be pointing to an actual file
-    let fileAttempt: vscode.Uri;
-    const attemptExtensions = [".tsx", ".ts", ".jsx", ".js"];
-    for (const ext of attemptExtensions) {
-        fileAttempt = vscode.Uri.joinPath(baseUri, "..", path + ext);
-        if (await doesUriExist(fileAttempt)) {
-            return fileAttempt.toString();
-        }
-    }
-
-    // Can't figure it out, just join it
     return simpleJoinedUri.toString();
 }
 
+// TODO: Ideally, the user should be able to add one or more config files of their choice
+async function findCompilerOptions(): Promise<CompilerOptions> {
+    const workspaceUri = vscode.workspace.workspaceFolders[0].uri;
+
+    // Try jsconfig.json
+    const jsConfigUri = vscode.Uri.joinPath(workspaceUri, "jsconfig.json");
+    const jsConfigExists = await doesUriExist(jsConfigUri);
+    if (jsConfigExists) {
+        const fileContent = await getFileContent(jsConfigUri);
+        const config = json5.parse(fileContent);
+        return config.compilerOptions ?? {};
+    }
+
+    // Try tsconfig.json
+    const tsConfigUri = vscode.Uri.joinPath(workspaceUri, "tsconfig.json");
+    const tsConfigExists = await doesUriExist(tsConfigUri);
+    if (tsConfigExists) {
+        const fileContent = await getFileContent(tsConfigUri);
+        const config = json5.parse(fileContent);
+        return config.compilerOptions ?? {};
+    }
+
+    // Blank compiler options
+    return {};
+}
+
 export async function getGlobalTradeInfo(uris: vscode.Uri[]) {
+    const compilerOptions = await findCompilerOptions();
+
     const globalTradeInfo: GlobalTradeInfo = { files: {} };
     const uriSet = new Set<vscode.Uri>(uris);
     const queue = Array.from(uris);
     while (queue.length !== 0) {
         const uriToBeChecked = queue.pop();
-
-        await addFileTradeInfo(uriToBeChecked, globalTradeInfo);
+        await addFileTradeInfo(
+            uriToBeChecked,
+            globalTradeInfo,
+            compilerOptions,
+        );
         const fti = globalTradeInfo.files[uriToBeChecked.toString()];
 
         // Maybe set it as an entry file
@@ -188,7 +320,14 @@ async function getFileContent(uri: vscode.Uri): Promise<string> {
         return cachedFileContent[uriString];
     }
 
-    if (!(await doesUriExist(uri))) {
+    // Ensure the URI exists, and it is a file
+    try {
+        const status = await vscodeFS.stat(uri);
+        if (status.type !== vscode.FileType.File) {
+            cachedFileContent[uriString] = "";
+            return cachedFileContent[uriString];
+        }
+    } catch {
         cachedFileContent[uriString] = "";
         return cachedFileContent[uriString];
     }
@@ -202,6 +341,7 @@ async function getFileContent(uri: vscode.Uri): Promise<string> {
 export async function addFileTradeInfo(
     uri: vscode.Uri,
     globalTradeInfo: GlobalTradeInfo,
+    compilerOptions: CompilerOptions,
 ) {
     const uriString = uri.toString();
 
@@ -234,6 +374,7 @@ export async function addFileTradeInfo(
                 const importSourceUriString = await vscodeResolve(
                     uri,
                     importSource,
+                    compilerOptions,
                 );
                 // true is meaningless
                 fileTradeInfo.dependencies[importSourceUriString] = true;
@@ -284,6 +425,7 @@ export async function addFileTradeInfo(
                     const importSourceUriString = await vscodeResolve(
                         uri,
                         importSource,
+                        compilerOptions,
                     );
                     // true is meaningless
                     fileTradeInfo.dependencies[importSourceUriString] = true;
